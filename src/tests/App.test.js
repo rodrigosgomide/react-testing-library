bd7ed13b8f43f14a73278163a9f2628c6f214fdd
rodrigosgomide <rodrigogomide.def@gmail.com>
2022-09-19 20:57:16 -0300

    R1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,46 @@
-test('start', () => {});
+import React from 'react';
+import { screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste App', () => {
+  it('Verifica o link Home', () => {
+    const { history } = renderWithRouter(<App />);
+    const links = screen.getAllByRole('link');
+    const linkHome = links[0];
+
+    expect(linkHome).toHaveTextContent('Home');
+    userEvent.click(linkHome);
+
+    const { location: { pathname } } = history;
+
+    expect(pathname).toBe('/');
+  });
+
+  it('Verifica o link About', () => {
+    const { history } = renderWithRouter(<App />);
+    const links = screen.getAllByRole('link');
+    const linkAbout = links[1];
+
+    expect(linkAbout).toHaveTextContent('About');
+    userEvent.click(linkAbout);
+
+    const { location: { pathname } } = history;
+
+    expect(pathname).toBe('/about');
+  });
+
+  it('Verifica o link About', () => {
+    const { history } = renderWithRouter(<App />);
+    const links = screen.getAllByRole('link');
+    const linkFavorite = links[2];
+
+    expect(linkFavorite).toHaveTextContent('Favorite Pokémons');
+    userEvent.click(linkFavorite);
+
+    const { location: { pathname } } = history;
+
+    expect(pathname).toBe('/favorites');
+  });
+});
