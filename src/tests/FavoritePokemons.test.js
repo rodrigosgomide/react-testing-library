a680aaa1b711a5d656e51900ee98e7aa9f246005
rodrigosgomide <rodrigogomide.def@gmail.com>
2022-09-20 18:15:32 -0300

    R4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { act } from 'react-dom/test-utils';
 import FavoritePokemons from '../pages/FavoritePokemons';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('Teste NotFound', () => {
+  it(
+    'Verifica se a mensagem "Page requested not found" é renderizada juntamente da img',
+    () => {
+      render(<NotFound />);
+      const heading = screen.getByRole('heading', {
+        name: 'Page requested not found',
+        level: 2,
+      });
+
+      expect(heading).toBeInTheDocument();
+
+      const image = screen.getByRole('img');
+
+      expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    },
+  );
+});
