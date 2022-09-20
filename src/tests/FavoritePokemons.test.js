d2e90f54d0166ce911b575b47a8dcd02541aa939
rodrigosgomide <rodrigogomide.def@gmail.com>
2022-09-20 18:04:50 -0300

    R3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste FavoritesPokemons', () => {
+  it('Verifica se o texto "No favorite pokemon found" é exibido na tela', () => {
+    render(<FavoritePokemons />);
+    const noFavorite = screen.getByText('No favorite pokemon found');
+    expect(noFavorite).toBeInTheDocument();
+  });
+
+  it(
+    'Verifica se ao adcionar um pokemon aos favoritos ele aparece na tela de favoritos',
+    () => {
+      renderWithRouter(<App />);
+      const details = screen.getByText(/more details/i);
+      userEvent.click(details);
+
+      const favorite = screen.getByRole('checkbox', {
+        checked: false,
+      });
+      userEvent.click(favorite);
+
+      const favorites = screen.getByText('Favorite Pokémons');
+      userEvent.click(favorites);
+
+      const favoritesPokemons = screen.getAllByText(/more details/i);
+      expect(favoritesPokemons.length).toBe(1);
+    },
+  );
+});
