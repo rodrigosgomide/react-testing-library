3fc3c3d28b381f36852602cc12df70c782b3e638
rodrigosgomide <rodrigogomide.def@gmail.com>
2022-09-20 15:10:02 -0300

    R2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import About from '../pages/About';
+
+describe('Teste About', () => {
+  it('Verifica se o componente possui um heading com o texto "About Pokédex"', () => {
+    render(<About />);
+    const headerAbout = screen.getByRole('heading', {
+      name: 'About Pokédex',
+      level: 2,
+    });
+
+    expect(headerAbout).toBeInTheDocument();
+  });
+
+  it('Verifica se o componente possuiu dois paragrafos sobre a Pokedex', () => {
+    render(<About />);
+    const pokedexParagraph = screen.getAllByText(/Pokémons/i);
+    expect(pokedexParagraph.length).toBe(2);
+  });
+
+  it('Verifica se o componente possui a imagem determianda', () => {
+    render(<About />);
+    const image = screen.getByRole('img');
+
+    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
